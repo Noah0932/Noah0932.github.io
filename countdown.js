50aef9f96c0ef5152e347652859915cb9c59e798
Noah0932 <dongankang090302@hotmail.com>
2025-08-18 22:21:52 +0800

    Site updated: 2025-08-18 22:21:51

--- countdown.js
+++ countdown.js
@@ -1,7 +1,7 @@
 const CountdownTimer = (() => {
     const config = {
-        targetDate: "2025-01-29",
-        targetName: "春节",
+        targetDate: "2025-09-01",
+        targetName: "开学",
         units: {
             day: { text: "今日", unit: "小时" },
             week: { text: "本周", unit: "天" },
